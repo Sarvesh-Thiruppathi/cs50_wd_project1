b5c17da250332573c7f2741fab46da8eea9cf3c2
Sarvesh Thiruppathi <sarveshthiruppathi@gmail.com>
2023-12-25 21:41:51 -0800

    added error page for no match found

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -10,6 +10,10 @@ def index(request):
     })
 
 def page(request, title):
+    if(util.get_entry(title) == None):
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
     return render(request, "encyclopedia/page.html", {
         "title": title,
         "content": markdown2.markdown(util.get_entry(title))
